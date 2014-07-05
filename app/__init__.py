5436a252fb4691218766c6b9302b054a5f820126
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2014-07-05 10:03:21 +0200

    working search

--- app/__init__.py
+++ app/__init__.py
@@ -8,19 +8,19 @@ from shutil import rmtree
 import os.path
 import whoosh
 import whoosh.fields, whoosh.index
+from whoosh.analysis import CharsetFilter, StemmingAnalyzer
+from whoosh.support.charset import accent_map
 from whoosh.query import *
-import pymongo
-from pymongo import MongoClient
+from whoosh.qparser import QueryParser, MultifieldParser
 import sys
 import requests
 from jsonschema import Draft3Validator
 import json
+from datetime import datetime
 
 whoosh_dir = join(environ["OPENSHIFT_DATA_DIR"],'index')
 
-
-#TODO: somethings phishy with pythons json load, as the results depends on the case of the boolean values
-ttn_schema = json.loads(open(join(environ["OPENSHIFT_REPO_DIR"],"app","spec","schema_true.json")).read())
+ttn_schema = json.loads(open(join(environ["OPENSHIFT_REPO_DIR"],"app","spec","schema.json")).read())
 validator = Draft3Validator(ttn_schema)
 
 if False or not exists(whoosh_dir):
@@ -28,13 +28,85 @@ if False or not exists(whoosh_dir):
         rmtree(whoosh_dir)
     mkdir(whoosh_dir)
 
-    schema = whoosh.fields.Schema(
-        thing = whoosh.fields.TEXT(stored = True),
-        subthing = whoosh.fields.TEXT()
+    tracker_schema = whoosh.fields.Schema(
+        url = whoosh.fields.ID(stored = True, unique = True),
+        description = whoosh.fields.TEXT(stored = True),
+        updated = whoosh.fields.DATETIME(),
+        accessed = whoosh.fields.DATETIME()
     )
-    ix = whoosh.index.create_in(whoosh_dir, schema)
+    tracker_idx = whoosh.index.create_in(whoosh_dir, tracker_schema, indexname = "trackers")
+
+    thing_schema = whoosh.fields.Schema(
+        id = whoosh.fields.ID(unique = True),
+        url = whoosh.fields.TEXT(stored = True),
+        title = whoosh.fields.TEXT(stored = True),
+        description = whoosh.fields.TEXT(),
+        authors = whoosh.fields.TEXT(),
+        licenses = whoosh.fields.TEXT(),
+        tags = whoosh.fields.TEXT()
+    )
+    thing_idx = whoosh.index.create_in(whoosh_dir, thing_schema, indexname = "things")
+
+
 else:
-    ix = whoosh.index.open_dir(whoosh_dir)
+    tracker_idx = whoosh.index.open_dir(whoosh_dir, indexname = "trackers")
+    thing_idx = whoosh.index.open_dir(whoosh_dir, indexname = "things")
+
+thing_parser = MultifieldParser(['title','description','tags','licenses'],schema = thing_idx.schema)
+tracker_parser = MultifieldParser(['description'],schema = tracker_idx.schema)
+
+def scan_tracker(url):
+    r = requests.get(url)
+
+    #skip unreachable trackers
+    if not r.status_code == requests.codes.ok:
+        print "Skipping unreachable tracker %s" % url
+        return
+
+    tracker = r.json()
+
+    if not validator.is_valid(tracker):
+        print "Skipping invalid tracker %s" % tracker["url"]
+        for error in validator.iter_errors(tracker):
+            print "%s not conforming to spec: %s" % (url,error.message)
+        return
+
+    with tracker_idx.searcher() as searcher:
+        if len(searcher.search(Term("url",url))) == 1:
+            print "Skipping known tracker %s" % url
+            return
+
+    with tracker_idx.writer() as writer:
+        writer.add_document(
+            url = tracker["url"],
+            description = tracker["description"],
+            accessed = datetime.now(),
+            updated = datetime.strptime(tracker["updated"][:-6],"%Y-%m-%dT%H:%M:%S")
+        )
+    if "things" in tracker:
+        with thing_idx.writer() as writer:
+            for thing in tracker["things"]:
+                for opt in ['url','description']:
+                    if not opt in thing:
+                        thing[opt] = u''
+                for opt in ['authors','licenses','tags']:
+                    if not opt in thing:
+                        thing[opt] = []
+
+                writer.update_document(
+                    id = thing["id"],
+                    url = thing["url"],
+                    title = thing["title"],
+                    description = thing["description"],
+                    authors = u" ".join([a["name"] for a in thing["authors"]]),
+                    licenses = u" ".join([l for l in thing["licenses"]]),
+                    tags = u", ".join([t for t in thing["tags"]])
+                )
+
+    if "trackers" in tracker:
+        for subtracker in tracker["trackers"]:
+            scan_tracker(subtracker["url"])
+
 
 app = Flask(__name__)
 app.secret_key = open(join(environ["OPENSHIFT_DATA_DIR"],"secret_key")).read()
@@ -42,16 +114,15 @@ app.secret_key = open(join(environ["OPENSHIFT_DATA_DIR"],"secret_key")).read()
 class SubmissionForm(Form):
     url = TextField("url",validators=[DataRequired(),URL(require_tld=True)])
 
+class SearchForm(Form):
+    query = TextField("query",validators=[DataRequired()])
+
+
 @app.route('/submit', methods=('GET', 'POST'))
 def submit():
     form = SubmissionForm()
     if form.validate_on_submit():
-        r = requests.get(form.url.data)
-        if not validator.is_valid(r.json()):
-            for error in validator.iter_errors(r.json()):
-                flash("%s not conforming to spec: %s" % (form.url.data,error.message))
-        else:
-            flash("%s successfully submitted" % form.url.data)
+        scan_tracker(form.url.data)
         return redirect("/submit")
     return render_template('submit.html', form=form)
 
@@ -59,40 +130,28 @@ def submit():
 def index():
     return redirect('/submit')
 
-#mongoclient = pymongo.MongoClient(environ["OPENSHIFT_MONGODB_DB_URL"])
-#db = mongoclient.database
+@app.route('/trackers')
+def list():
+    searcher = tracker_idx.searcher()
+    trackers = [tracker for tracker in searcher.all_stored_fields()]
+    return render_template('list.html',trackers=trackers)
 
-#@app.route('/eimer/clear')
-#def clear_collection():
-#    db.eimer.drop()
+@app.route('/things')
+def things():
+    searcher = thing_idx.searcher()
+    things = [thing for thing in searcher.all_stored_fields()]
+    return render_template('things.html',things=things)
 
-#@app.route('/eimer/add/<thing>/<subthing>')
-#def add_to_collection(thing,subthing):
-##    db.eimer.insert({"thing" : thing, "subthing" : subthing})
-#
-#    #TODO: handle non-thread safety
-#    writer = ix.writer()
-#    writer.add_document(thing=thing, subthing=subthing)
-#    writer.commit()
-#    return "%s added" % thing
-
-#@app.route('/eimer/view')
-#def view_collection():
-#    return " ".join([doc["thing"] for doc in db.eimer.find()])
-
-#@app.route('/eimer/search/<term>')
-#def search_collection(term):
-#    with ix.searcher() as searcher:
-#        query = Or([Term("thing",term), Term("subthing",term)])
-#        results = searcher.search(query)
-#        n = min(20,len(results))
-#        print n
-#        print results[0]["thing"]
-#        if n == 0:
-#            return "No results found"
-#        else:
-#            return " ".join([str(results[i]) for i in range(n)])
-
+@app.route('/search', methods=('GET', 'POST'))
+def search():
+    results = []
+    form = SearchForm()
+    if form.validate_on_submit():
+        with thing_idx.searcher() as searcher:
+            hits = searcher.search(thing_parser.parse(form.query.data))
+            for i in range(hits.scored_length()):
+                results.append(hits[i].fields())
+    return render_template('search.html', form=form, results=results)
 
 if __name__ == '__main__':
     app.run()
